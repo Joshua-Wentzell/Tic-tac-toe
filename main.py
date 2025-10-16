e7dd66c64e7f289ae2c59bf160c6c6a75cfdfad4
jwentzel <wentzelljoshua047@gmail.com>
2025-10-15 21:51:11 -0400

    Add enum values for cell states. Add method to update game state with a new move.

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+from enum import Enum
+
+
+class CellValue(Enum):
+    EMPTY = 1
+    PLAYER1 = 2
+    PLAYER2 = 3
+
+
 class GameState:
     def __init__(self):
         self.board_data = {
-            "A1": False,
-            "A2": False,
-            "A3": False,
-            "B1": False,
-            "B2": False,
-            "B3": False,
-            "C1": False,
-            "C2": False,
-            "C3": False
+            "A1": CellValue.EMPTY,
+            "A2": CellValue.EMPTY,
+            "A3": CellValue.EMPTY,
+            "B1": CellValue.EMPTY,
+            "B2": CellValue.EMPTY,
+            "B3": CellValue.EMPTY,
+            "C1": CellValue.EMPTY,
+            "C2": CellValue.EMPTY,
+            "C3": CellValue.EMPTY,
         }
-    
+
     def __repr__(self):
         data_string = ""
         sorted_data = sorted(self.board_data.items(), key=lambda item: item[0])
@@ -23,10 +32,24 @@ class GameState:
                 data_string += "\n"
                 last_row_letter = data_point[0][0]
             value = " "
-            if data_point[1] == True:
-                value = "X"
+            match data_point[1]:
+                case CellValue.EMPTY:
+                    value = " "
+                case CellValue.PLAYER1:
+                    value = "X"
+                case CellValue.PLAYER2:
+                    value = "O"
             data_string += f"[{value}]"
         return data_string
 
+    def make_move(self, cell, value):
+        if cell in self.board_data.keys():
+            self.board_data[cell] = value
+
+
 new_game = GameState()
+print(new_game)
+new_game.make_move("B2", CellValue.PLAYER1)
+print(new_game)
+new_game.make_move("C1", CellValue.PLAYER2)
 print(new_game)
